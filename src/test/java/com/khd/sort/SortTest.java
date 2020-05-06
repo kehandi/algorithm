d2fcbb14cc5897a876b67ee277021d384c5e7505
KeHanDi <824592535@qq.com>
2020-05-06 11:19:23 +0800

    测试断言

--- src/test/java/com/khd/sort/SortTest.java
+++ src/test/java/com/khd/sort/SortTest.java
@@ -1,17 +1,20 @@
 package com.khd.sort;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * @author mac
  * @title: SortTest
  * @projectName algorithm
- * @description: TODO
+ * @description: 排序测试
  * @date 2020-05-06 10:54
  */
 public class SortTest {
 
-    public static int[] array = {4, 6, 1, 8, 2, 7, 6};
+    private static int[] array = {4, 6, 1, 8, 2, 7, 6};
+
+    private static int[] expectedArray = {1, 2, 4, 6, 6, 7, 8};
 
     @Test
     public void TestBubbleSort() {
@@ -19,13 +22,18 @@ public class SortTest {
         Sort sort = new BubbleSort();
         sort.sort(array);
         sort.printArray(array);
+
+        Assert.assertArrayEquals("排序结果与预期不符", expectedArray, array);
+
     }
 
     @Test
-    public void TestSelectionSort () {
+    public void TestSelectionSort() {
         // 测试选择排序
         Sort sort = new SelectionSort();
         sort.sort(array);
         sort.printArray(array);
+
+        Assert.assertArrayEquals("排序结果与预期不符", expectedArray, array);
     }
 }
